d9681de8b814da7cb481064876b766bd0c954931
Yinyi Wang <ywang2@linkedin.com>
2015-06-01 21:51:04 -0700

    added comparison and auto detect big percentile changes

--- liquid_perf_web/public/views/query.js
+++ liquid_perf_web/public/views/query.js
@@ -3,7 +3,7 @@ function HttpGet(path, callback) {
     var xmlhttp;
     // code for IE7+, Firefox, Chrome, Opera, Safari
     xmlhttp=new XMLHttpRequest();
-    
+
     xmlhttp.onreadystatechange=function() {
         if (xmlhttp.readyState==4 && xmlhttp.status==200)
             {
@@ -39,7 +39,7 @@ function drawChart(rows) {
     }
 
     data.addRows(jsrows);
-    
+
     // Set chart options
     var options = {'title':'How Much Pizza I Ate Last Night',
                    'width':1200,
@@ -80,7 +80,7 @@ function fillAllRuns(runs, select_id) {
         selector.appendChild(option);
     };
 };
-    
+
 
 // show the runtime of a given test
 function handleTestChange() {
@@ -91,7 +91,7 @@ function handleTestChange() {
             data.addColumn('string', 'date');
             data.addColumn('number', 'seconds');
             // data.addColumn('number', 'cpu cycle');
-            
+
             var jsrows = [];
             var i;
             for (i=0; i < rows.length; i++) {
@@ -104,7 +104,7 @@ function handleTestChange() {
                            'width':1200,
                            'height':900
             };
-             
+
             // Instantiate and draw our chart, passing in some options.
             var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
             chart.draw(data, options);
@@ -113,14 +113,99 @@ function handleTestChange() {
 
 // update the perf results of selected runs on the left
 function handleLeftRunChange() {
-    var hash = document.getElementById("left_run_sel").value;
+    handleLeftOrRightChange("left");
+    return;
+   var hash = document.getElementById("left_run_sel").value;
     HttpGet("/getAllPerfFromRun?run_hash=" + hash, function(rows) {
-            /*
-            var div = document.getElementById('comp_left');
+            var div = document.getElementById("comp_left");
             div.innerHTML = '';
             for (var i=0; i<rows.length; i++) {
-                div.innerHTML = rows[i].div.innerHTML + 'Extra stuff';
+                div.innerHTML += '<br><b>' + rows[i].testname + '</b><br>';
+                div.innerHTML += '<br>';
+                var functions = rows[i].functions;
+                for (var j=0; j<functions.length; j++) {
+                    div.innerHTML += functions[j].percentage + '%  ' + functions[j].funcname + '<br>';
+                }
             }
-            */
+            div.innerHTML += '<br>';
+        });
+    showNewComparison();
+}
+
+function handleRightRunChange() {
+    handleLeftOrRightChange("right");
+}
+
+function handleLeftOrRightChange(direction) {
+    var hash = document.getElementById(direction + "_run_sel").value;
+    HttpGet("/getAllPerfFromRun?run_hash=" + hash, function(rows) {
+            var div = document.getElementById("comp_" + direction);
+            div.innerHTML = '';
+            for (var i=0; i<rows.length; i++) {
+                div.innerHTML += '<br><b>' + rows[i].testname + '</b><br>';
+                div.innerHTML += '<br>';
+                var functions = rows[i].functions;
+                for (var j=0; j<functions.length && j<15; j++) {
+                    div.innerHTML += functions[j].percentage + '%  ' + functions[j].funcname + '<br>';
+                }
+            }
+            div.innerHTML += '<br>';
+        });
+    showNewComparison();
+}
+
+function showNewComparison() {
+    var left_hash = document.getElementById("left_run_sel").value;
+    var right_hash = document.getElementById("right_run_sel").value;
+    HttpGet("/getAllPerfFromRun?run_hash=" + left_hash, function(left_rows) {
+            HttpGet("/getAllPerfFromRun?run_hash=" + right_hash, function(right_rows) {
+                    var div = document.getElementById("comp_result");
+                    var left_testtable = {};
+                    for (var i=0; i<left_rows.length; i++) {
+                        var testname = left_rows[i]["testname"];
+                        left_testtable[testname] = left_rows[i].functions;
+                    }
+                    var right_testtable = {};
+                    for (var i=0; i<right_rows.length; i++) {
+                        right_testtable[right_rows[i]["testname"]] = right_rows[i].functions;
+                    }
+                    /*
+                    for (var i=0; i<right_rows.length; i++) {
+                        if(testtable.hasOwnProperty(right_rows[i]["testname"])) {
+                            testtable[right_rows[i].testname] = 2;
+                        }
+                    }
+                    */
+                    for (var test in left_testtable) {
+                        //div.innerHTML += ' found one test in left ' + test;
+                        if (right_testtable.hasOwnProperty(test)) {
+                            //div.innerHTML += 'found one test in right also' + test;
+                            // analyze diff of the two arrays
+                            var left = left_testtable[test];
+                            var right =right_testtable[test];
+                            // store all left functions into a map
+                            var left_map = {};
+                            for (var j=0; j<left.length; j++) {
+                                left_map[left[j].funcname] = left[j].percentage;
+                            }
+                            var min_len = Math.min(left.length, right.length);
+                            // check from the right
+                            for (var j=0; j < right.length; j++) {
+                                var func = right[j].funcname;
+                                //div.innerHTML += 'probing using function: ' + func;
+                                if (left_map.hasOwnProperty(func)) {
+                                    var right_percent = right[j].percentage;
+                                    var left_percent = left_map[func];
+                                    //div.innerHTML += 'function ' + func + ' left:' + left_percent + ' right:' + right_percent;
+                                    if (right_percent > 5 && right_percent > left_percent && ((right_percent - left_percent)*1.0/left_percent > 0.4)) {
+                                        div.innerHTML += '<br>Significant change: in ' + test + ', function ' + func + 
+                                            ' runtime percentage has increased from left:' + left_percent + ' to right:' + right_percent;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+                });
         });
 }
